1741a59ed627a8f917c06adc142276015c8d35e1
StfaniLopez <stfanilopez@gmail.com>
2025-08-06 15:02:20 -0600

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Conexión usando el usuario root, DB contabilidad y charset utf8mb4
-DATABASE_URL = "mysql+pymysql://root:ContaEntregable04@localhost:3306/contabilidad?charset=utf8mb4"
+DATABASE_URL = "mysql+pymysql://root:Contrasena@localhost:3306/contabilidad?charset=utf8mb4"
 
 engine = create_engine(
     DATABASE_URL,
